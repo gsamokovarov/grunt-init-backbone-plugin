4f580f3d8bf6ffa96ad5786c89a7e2120571bfb3
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-02-03 17:17:57 +0200

    Strip backbone prefix from backbone-name

--- root/backbone-name.js
+++ root/backbone-name.js
@@ -1,4 +1,4 @@
-// backbone-{%= name %} {%= version %}
+// {%= name %} {%= version %}
 //
 // (c) {%= grunt.template.today('yyyy') %} {%= author_name %}
 // Licensed under the {%= licenses.join(', ') %} license{%= licenses.length === 1 ? '' : 's' %}.
