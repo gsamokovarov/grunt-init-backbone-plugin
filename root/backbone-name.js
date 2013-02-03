f5cf6cc2b09779c61f3823cbd1d57c857f27ff9a
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-02-03 15:40:24 +0200

    Add a plugin core scaffold boilerplate

---
+++ root/backbone-name.js
@@ -0,0 +1,10 @@
+// backbone-{%= name %} {%= version %}
+//
+// (c) {%= grunt.template.today('yyyy') %} {%= author_name %}
+// Licensed under the {%= licenses.join(', ') %} license{%= licenses.length === 1 ? '' : 's' %}.
+
+(function(Backbone) {
+
+  // Your code goes here...
+
+}).call(this, Backbone);
